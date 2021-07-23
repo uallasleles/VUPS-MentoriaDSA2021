0ecadad6891b376cbdacd2eaf034e13fd7a28e12
Uallas Leles <57737232+uallasleles@users.noreply.github.com>
2021-07-23 08:50:49 -0300

    imports

--- dash/app.py
+++ dash/app.py
@@ -1,49 +1,7 @@
-# Instale o Dash
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as pe
+import pandas as pd
 
-# > pip install dash
-
-# Isso traz três bibliotecas de componentes que constituem o núcleo do Dash:
-
-# - dash_html_components
-# - dash_core_components
-# - dash_table
-
-# bem como a biblioteca de gráficos plotados, 
-
-# - plotly. 
-
-# Essas bibliotecas estão em desenvolvimento ativo, 
-# portanto, instale e atualize com frequência.
-
-# ----------------------------------------------------------------------------
-
-# Se você preferir Jupyter notebook ou JupyterLab como seu ambiente de desenvolvimento, 
-# recomendamos instalar o jupyter-dash:
-
-# > pip install jupyter-dash
-
-# Também recomendamos a instalação do Pandas, que é exigido pelo Plotly Express e 
-# usado em muitos de nossos exemplos.
-
-# > pip install pandas
-
-
-# Dash Layout
-# ============================================================================
-
-# > Para aplicativos Dash de produção, 
-# > recomendamos estilizar o layout do aplicativo 
-# > com o Dash Enterprise Design Kit.
-
-# Os aplicativos do Dash são compostos de duas partes. 
-# 1. A primeira parte é o "layout" do aplicativo e descreve a aparência do aplicativo. 
-# 2. A segunda parte descreve a interatividade do aplicativo.
-
-# Dash fornece classes Python para todos os componentes visuais do aplicativo. 
-# Mantemos um Conjunto de Componentes nas bibliotecas 
-# - dash_core_components e 
-# - dash_html_components, 
-# mas você também pode construir o seu próprio com 
-# - JavaScript e 
-# - React.js.
-
+#
