056fda9bb53ad8a2f9d163a9fd9e2f4aa216eae3
Uallas Leles <uallasleles@hotmail.com>
2021-08-22 12:54:58 -0300

    Update st_start.py

--- st_start.py
+++ st_start.py
@@ -10,11 +10,12 @@ st.title('Meu primeiro App Streamlit!')
 
 st.write('## **Teste markdown!**')
 
+
 microdados = vups.datasets.microdados(nrows=1000)
 st.dataframe(microdados)
 fig1 = vups.plot_bar()
-fig1
-
+fig2 = vups.plot_year_taxs()
+fig2
 # Desenhe um gráfico de linha
 # ============================================================================
 # Você pode adicionar facilmente um gráfico de linha ao seu aplicativo com st.line_chart (). 
@@ -151,3 +152,21 @@ for i in range(100):
 #   - Clique em "Implementar um aplicativo" e cole no URL do GitHub
 
 # ----------------------------------------------------------------------------
+
+st.text('This will appear first')
+# Appends some text to the app.
+
+my_slot1 = st.empty()
+# Appends an empty slot to the app. We'll use this later.
+
+my_slot2 = st.empty()
+# Appends another empty slot.
+
+st.text('This will appear last')
+# Appends some more text to the app.
+
+my_slot1.text('This will appear second')
+# Replaces the first empty slot with a text string.
+
+my_slot2.line_chart(np.random.randn(20, 2))
+# Replaces the second empty slot with a chart.
