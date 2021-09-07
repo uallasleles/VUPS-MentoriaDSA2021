097d946017cf02b72c47d222add3c6a77328866b
Uallas Leles <uallasleles@hotmail.com>
2021-09-07 14:03:33 -0300

    fixing .gitignore

--- app/home/routes.py
+++ app/home/routes.py
@@ -68,19 +68,23 @@ def dashboard():
         graphJSON5=graphsJSON[5],
         graphJSON6=graphsJSON[6],
         graphJSON7=graphsJSON[7],
+        graphJSON8=graphsJSON[8],
         )
 
-def getJSON(UF='ES'):
-    fig0 = vups.plot_year_taxs(UF=UF)
-    fig1 = vups.plot_comp_tributos_cidades_norm()
-    fig2 = vups.plot_calendar_heatmap()
-    fig3 = vups.plot_map_folium()
-    fig4 = graphs.plot_kpi_percentage_progress()
-    fig5 = graphs.plot_kpi_spend_hours()
-    fig6 = graphs.plot_kpi_tcpi()
-    fig7 = graphs.plot_line_progress_actual_planned()
+graphsJSON = []
+def getJSON():
+    fig_lst = [
+        vups.plot_year_taxs(UF=UF),
+        vups.plot_comp_tributos_cidades_norm(),
+        vups.plot_calendar_heatmap(),
+        vups.plot_map_folium(),
+        graphs.plot_kpi_percentage_progress(),
+        graphs.plot_kpi_spend_hours(),
+        graphs.plot_kpi_tcpi(),
+        graphs.plot_line_progress_actual_planned(),
+        vups.plot_tributos_ipca(),
+        ]
 
-    graphsJSON = []
     graphsJSON.append(json.dumps(fig0, cls=utils.PlotlyJSONEncoder))
     graphsJSON.append(json.dumps(fig1, cls=utils.PlotlyJSONEncoder))
     graphsJSON.append(json.dumps(fig2, cls=utils.PlotlyJSONEncoder))
@@ -89,5 +93,54 @@ def getJSON(UF='ES'):
     graphsJSON.append(json.dumps(fig5, cls=utils.PlotlyJSONEncoder))
     graphsJSON.append(json.dumps(fig6, cls=utils.PlotlyJSONEncoder))
     graphsJSON.append(json.dumps(fig7, cls=utils.PlotlyJSONEncoder))
+    graphsJSON.append(json.dumps(fig8, cls=utils.PlotlyJSONEncoder))
 
     return graphsJSON
+
+# KPI's
+def get_plot_kpi_percentage_progress():
+    return graphs.plot_kpi_percentage_progress()
+
+def get_plot_kpi_spend_hours():
+    return graphs.plot_kpi_spend_hours()
+
+def get_plot_kpi_tcpi():
+    return graphs.plot_kpi_tcpi()
+
+# Pimpão
+def get_plot_comp_tributos_cidades():
+    return vups.plot_comp_tributos_cidades()
+
+def get_plot_comp_tributos_cidades_norm():
+    return vups.plot_comp_tributos_cidades_norm()
+
+def get_plot_calendar_heatmap():
+    return vups.plot_calendar_heatmap()
+
+def get_plot_tributos_ipca():
+    return vups.plot_tributos_ipca()
+
+# Uallas
+def get_plot_year_taxs(UF='ES'):
+    return vups.plot_year_taxs(UF=UF)
+
+def get_plot_map_folium():
+    return vups.plot_map_folium()
+
+# Small Bars
+def get_plot_small_bar_percentage_progress():
+    return graphs.plot_small_bar_percentage_progress()
+
+def get_plot_small_bar_spend_hours():
+    return graphs.plot_small_bar_spend_hours()
+
+# Line Plot - Progresso Atual vs Planejado
+def get_plot_line_progress_actual_planned():
+    return graphs.plot_line_progress_actual_planned()
+
+# Widgets
+def get_plot_widget_cost_variance():
+    return graphs.plot_widget_cost_variance()
+
+def get_plot_widget_schedule_variance()():
+    return graphs.plot_widget_schedule_variance()
