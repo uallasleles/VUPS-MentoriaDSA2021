de95a7ec3fc16b062da987850a62ed4bb66fde5c
Uallas Leles <uallasleles@hotmail.com>
2021-09-09 01:28:13 -0300

    configurando métodos par cada plot

--- app/home/routes.py
+++ app/home/routes.py
@@ -12,34 +12,38 @@ from plotly import utils
 from jinja2 import TemplateNotFound
 
 
+
+##############################################################################
+# ROTEAMENTO DAS PÁGINAS
+##############################################################################
+
+@blueprint.route('/')
 @blueprint.route('/index')
 def index():
     return render_template('index.html', segment='index')
 
 
-@blueprint.route('/st')
-def st():
-    return redirect(url_for('streamlit.py'))
+@blueprint.route('/dashboard')
+@blueprint.route('/dashboard.html')
+def dashboard():
+    progress_actual_planned = get_plot_line_progress_actual_planned()
+    return render_template('dashboard.html', segment='dashboard', progress_actual_planned = progress_actual_planned)
 
 
 @blueprint.route('/<template>')
 def route_template(template):
+    print('/<template>')
     try:
         if not template.endswith( '.html' ):
             template += '.html'
-            
         # Detect the current page
         segment = get_segment( request )
-
         # Serve the file (if exists) from app/templates/FILE.html
         return render_template( template, segment=segment )
-
     except TemplateNotFound:
         return render_template('page-404.html'), 404
-    
     except:
         return render_template('page-500.html'), 500
-
 
 # Helper - Extract current page name from request 
 def get_segment( request ): 
@@ -52,95 +56,95 @@ def get_segment( request ):
         return None
 
 
-@blueprint.route('/callback', methods=['POST', 'GET'])
-def cb():
-    return getJSON(request.args.get('data'))[0]
+@blueprint.route('/refresh_progress_actual_planned', methods=['POST', 'GET'])
+def refresh_progress_actual_planned():
+    return get_plot_line_progress_actual_planned()
 
-@blueprint.route('/dashboard')
-def dashboard():
-    graphsJSON=getJSON()
-    return render_template('dashboard.html', segment='dashboard', 
-        graphJSON0=graphsJSON[0], 
-        graphJSON1=graphsJSON[1],
-        graphJSON2=graphsJSON[2],
-        graphJSON3=graphsJSON[3],
-        graphJSON4=graphsJSON[4],
-        graphJSON5=graphsJSON[5],
-        graphJSON6=graphsJSON[6],
-        graphJSON7=graphsJSON[7],
-        graphJSON8=graphsJSON[8],
-        )
 
-graphsJSON = []
-def getJSON():
-    fig_lst = [
-        vups.plot_year_taxs(UF=UF),
-        vups.plot_comp_tributos_cidades_norm(),
-        vups.plot_calendar_heatmap(),
-        vups.plot_map_folium(),
-        graphs.plot_kpi_percentage_progress(),
-        graphs.plot_kpi_spend_hours(),
-        graphs.plot_kpi_tcpi(),
-        graphs.plot_line_progress_actual_planned(),
-        vups.plot_tributos_ipca(),
-        ]
+@blueprint.route('/refresh_kpi')
+def refresh_kpi():
+    percentage_progress = get_plot_kpi_percentage_progress()
+    spend_hours = get_plot_kpi_spend_hours()
+    tcpi = get_plot_kpi_tcpi()
+    return percentage_progress, spend_hours, tcpi
 
-    graphsJSON.append(json.dumps(fig0, cls=utils.PlotlyJSONEncoder))
-    graphsJSON.append(json.dumps(fig1, cls=utils.PlotlyJSONEncoder))
-    graphsJSON.append(json.dumps(fig2, cls=utils.PlotlyJSONEncoder))
-    graphsJSON.append(json.dumps(fig3, cls=utils.PlotlyJSONEncoder))
-    graphsJSON.append(json.dumps(fig4, cls=utils.PlotlyJSONEncoder))
-    graphsJSON.append(json.dumps(fig5, cls=utils.PlotlyJSONEncoder))
-    graphsJSON.append(json.dumps(fig6, cls=utils.PlotlyJSONEncoder))
-    graphsJSON.append(json.dumps(fig7, cls=utils.PlotlyJSONEncoder))
-    graphsJSON.append(json.dumps(fig8, cls=utils.PlotlyJSONEncoder))
-
-    return graphsJSON
+##############################################################################
+# FUNÇÕES PARA DUMPS DE PLOTS EM JSON
+##############################################################################
 
 # KPI's
 def get_plot_kpi_percentage_progress():
-    return graphs.plot_kpi_percentage_progress()
+    return json.dumps(
+        obj = graphs.plot_kpi_percentage_progress(), 
+        cls = utils.PlotlyJSONEncoder)
 
 def get_plot_kpi_spend_hours():
-    return graphs.plot_kpi_spend_hours()
+    return json.dumps(
+        obj = graphs.plot_kpi_spend_hours(), 
+        cls = utils.PlotlyJSONEncoder)
 
 def get_plot_kpi_tcpi():
-    return graphs.plot_kpi_tcpi()
+    return json.dumps(
+        obj = graphs.plot_kpi_tcpi(), 
+        cls = utils.PlotlyJSONEncoder)
 
 # Pimpão
 def get_plot_comp_tributos_cidades():
-    return vups.plot_comp_tributos_cidades()
+    return json.dumps(
+        obj = vups.plot_comp_tributos_cidades(), 
+        cls = utils.PlotlyJSONEncoder)
 
 def get_plot_comp_tributos_cidades_norm():
-    return vups.plot_comp_tributos_cidades_norm()
+    return json.dumps(
+        obj = vups.plot_comp_tributos_cidades_norm(), 
+        cls = utils.PlotlyJSONEncoder)
 
 def get_plot_calendar_heatmap():
-    return vups.plot_calendar_heatmap()
+    return json.dumps(
+        obj = vups.plot_calendar_heatmap(), 
+        cls = utils.PlotlyJSONEncoder)
 
 def get_plot_tributos_ipca():
-    return vups.plot_tributos_ipca()
+    return json.dumps(
+        obj = vups.plot_tributos_ipca(), 
+        cls = utils.PlotlyJSONEncoder)
 
 # Uallas
 def get_plot_year_taxs(UF='ES'):
-    return vups.plot_year_taxs(UF=UF)
+    return json.dumps(
+        obj = vups.plot_year_taxs(UF=UF), 
+        cls = utils.PlotlyJSONEncoder)
 
 def get_plot_map_folium():
-    return vups.plot_map_folium()
+    return json.dumps(
+        obj = vups.plot_map_folium(),
+        cls = utils.PlotlyJSONEncoder)
 
 # Small Bars
 def get_plot_small_bar_percentage_progress():
-    return graphs.plot_small_bar_percentage_progress()
+    return json.dumps(
+        obj = graphs.plot_small_bar_percentage_progress(),
+        cls = utils.PlotlyJSONEncoder)
 
 def get_plot_small_bar_spend_hours():
-    return graphs.plot_small_bar_spend_hours()
+    return json.dumps(
+        obj = graphs.plot_small_bar_spend_hours(),
+        cls = utils.PlotlyJSONEncoder)
 
 # Line Plot - Progresso Atual vs Planejado
 def get_plot_line_progress_actual_planned():
-    return graphs.plot_line_progress_actual_planned()
+    graphJSON = json.dumps(
+        obj = graphs.plot_line_progress_actual_planned(),
+        cls = utils.PlotlyJSONEncoder)
+    return graphJSON
 
 # Widgets
 def get_plot_widget_cost_variance():
-    return graphs.plot_widget_cost_variance()
+    return json.dumps(
+        obj = graphs.plot_widget_cost_variance(),
+        cls = utils.PlotlyJSONEncoder)
 
 def get_plot_widget_schedule_variance():
-    return graphs.plot_widget_schedule_variance()
+    return json.dumps(
+        obj = graphs.plot_widget_schedule_variance(),
+        cls = utils.PlotlyJSONEncoder)
