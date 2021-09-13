803871136bf475f11e2d98d43dd3c1c6d141947f
Uallas Leles <uallasleles@hotmail.com>
2021-09-13 04:04:01 -0300

    Criada classe para importar CSV Microdados

--- app/base/models.py
+++ app/base/models.py
@@ -4,7 +4,7 @@ Programa de Mentoria DSA 2021
 """
 
 from flask_login import UserMixin
-from sqlalchemy import Binary, Column, Integer, String
+from sqlalchemy import Binary, Column, Integer, String, Date
 from app import db, login_manager
 from app.base.util import hash_pass
 
@@ -34,6 +34,77 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return str(self.username)
+
+
+class Microdados(db.Model):
+    
+    __tablename__ = 'Microdados'
+    #__table_args__ = {'sqlite_autoincrement': True}
+
+    id = Column(Integer, primary_key=True, nullable=False)
+
+    DataNotificacao = Column(String)
+    DataCadastro = Column(String)
+    DataDiagnostico = Column(String)
+    DataColeta_RT_PCR = Column(String)
+    DataColetaTesteRapido = Column(String)
+    DataColetaSorologia = Column(String)
+    DataColetaSorologiaIGG = Column(String)
+    DataEncerramento = Column(String)
+    DataObito = Column(String)
+
+    Classificacao = Column(String)
+    Evolucao = Column(String)
+    CriterioConfirmacao = Column(String)
+    StatusNotificacao = Column(String)
+    Municipio = Column(String)
+    Bairro = Column(String)
+    FaixaEtaria = Column(String)
+    IdadeNaDataNotificacao = Column(String)
+    Sexo = Column(String)
+    RacaCor = Column(String)
+    Escolaridade = Column(String)
+    Gestante = Column(String)
+    Febre = Column(String)
+    DificuldadeRespiratoria = Column(String)
+    Tosse = Column(String)
+    Coriza = Column(String)
+    DorGarganta = Column(String)
+    Diarreia = Column(String)
+    Cefaleia = Column(String)
+
+    ComorbidadePulmao = Column(String)
+    ComorbidadeCardio = Column(String)
+    ComorbidadeRenal = Column(String)
+    ComorbidadeDiabetes = Column(String)
+    ComorbidadeTabagismo = Column(String)
+    ComorbidadeObesidade = Column(String)
+
+    FicouInternado = Column(String)
+    ViagemBrasil = Column(String)
+    ViagemInternacional = Column(String)
+    ProfissionalSaude = Column(String)
+    PossuiDeficiencia = Column(String)
+    MoradorDeRua = Column(String)
+    ResultadoRT_PCR = Column(String)
+    ResultadoTesteRapido = Column(String)
+    ResultadoSorologia = Column(String)
+    ResultadoSorologia_IGG = Column(String)
+    TipoTesteRapido = Column(String)
+
+    def __init__(self, **kwargs):
+        for property, value in kwargs.items():
+            # depending on whether value is an iterable or not, we must
+            # unpack it's value (when **kwargs is request.form, some values
+            # will be a 1-element list)
+            if hasattr(value, '__iter__') and not isinstance(value, str):
+                # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
+                value = value[0]
+    
+            setattr(self, property, value)
+        
+        def __repr__(self):
+            return str(self.Municipio)
 
 
 @login_manager.user_loader
