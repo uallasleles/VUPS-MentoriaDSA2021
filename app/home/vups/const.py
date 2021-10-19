9b545da5ea35beea2aadff1f0a295eef147a54a5
Uallas Leles <uallasleles@hotmail.com>
2021-10-19 11:29:33 -0300

    Melhora no mapeamento dos tipos de dados através dos arquivos DTYPE_MAP para cada dataset.

--- app/home/vups/const.py
+++ app/home/vups/const.py
@@ -1,4 +1,7 @@
-# Módulo de valores constantes
+# -*- encoding: utf-8 -*-
+"""
+* MÓDULO: const
+"""
 
 # Imports
 import os
@@ -7,10 +10,8 @@ import json
 # Constantes
 BASEDIR = os.path.dirname(os.path.dirname(__file__))
 DATADIR = os.path.join(BASEDIR, "data")
+DATATMP = os.path.join(DATADIR, "tmp")
 METADIR = os.path.join(DATADIR, "metadata")
-
-APP_LOGO = "imagens/logo.png"
-MAPPING_FILE = "config/mapeamento_campos_dataset.json"
 
 # ABSPATH = os.path.abspath('.')
 # BASENAME = os.path.basename('.')
@@ -19,133 +20,32 @@ MAPPING_FILE = "config/mapeamento_campos_dataset.json"
 # DIRNAME = os.path.dirname(__file__)
 # REALPATH = os.path.realpath('.')
 
-# Inicializa os nomes das colunas do dataset
-ID = "ID"
-STATUS = "Status"
-CRIADO_POR = "Criado Por"
-ATRIBUIDO_A = "Atribuido A"
-ATENDIDO_POR = "Atendido Por"
-SEVERIDADE = "Severidade"
-PRIORIDADE = "Prioridade"
-CLIENTE = "Cliente"
-DATA_CRIACAO = "Data Criação"
+# ############################################################################
+# TRIAGEM DOS TIPOS DE DADOS
+# ############################################################################
 
-# Nomes no arquivo csv
-CSV_ID = "ID"
-CSV_STATUS = "Status"
-CSV_CRIADO_POR = "Criado Por"
-CSV_ATRIBUIDO_A = "Atribuido A"
-CSV_ATENDIDO_POR = "Atendido Por"
-CSV_SEVERIDADE = "Severidade"
-CSV_PRIORIDADE = "Prioridade"
-CSV_CLIENTE = "Cliente"
-CSV_DATA_CRIACAO = "Data Criação"
+def read_dtype_map(DS_NAME):
+    DTYPE_DICT = {
+        "date_cols": [],
+        "cat_cols": [],
+        "float_cols": [],
+        "int_cols": [],
+        }
 
-# Dicionário de mapeamento dos campos
-FIELD_MAP = {
-    "ID": "ID",
-    "Status": "Status",
-    "Criado Por": "Criado Por",
-    "Atribuido A": "Atribuido A",
-    "Atendido Por": "Atendido Por",
-    "Severidade": "Severidade",
-    "Prioridade": "Prioridade",
-    "Cliente": "Cliente",
-    "Data Criação": "Data Criação",
-}
+    try:
+        with open(os.path.join(METADIR, DS_NAME)) as f:
+            DTYPE_MAP = json.load(f)
 
-# Formato de data
-DATE_FORMAT = "%d-%m-%Y %H:%M"
+        for key, value in DTYPE_MAP["DTypeMap"].items():
+            if value == 'datetime64[ns]':   DTYPE_DICT["date_cols"].append(key)
+            if value == 'float64':          DTYPE_DICT["float_cols"].append(key)
+            if value == 'int64':            DTYPE_DICT["int_cols"].append(key)
+            if value == 'category':         DTYPE_DICT["cat_cols"].append(key)
+            
+    except:
+        pass
 
-# Variáveis customizadas
-CREATED_TIME = "CreatedTime"
-CREATED_DT = "CreatedDT"
-STATUS_TYPE = "StatusType"
-CLOSED_ISSUE_STATUS = [
-    "Fechado",
-    "Resolvido",
-    "Solução Proposta",
-    "Pesquisa Realizada",
-    "Solução Aplicada",
-    "Solução Documentada",
-]
-
-# Função para leitura das configurações
-def read_config():
-
-    # Variáveis
-    global ID
-    global STATUS
-    global CRIADO_POR
-    global ATRIBUIDO_A
-    global ATENDIDO_POR
-    global SEVERIDADE
-    global PRIORIDADE
-    global CLIENTE
-    global DATA_CRIACAO
-    global DATA_FECHAMENTO
-
-    global CSV_ID
-    global CSV_STATUS
-    global CSV_CRIADO_POR
-    global CSV_ATRIBUIDO_A
-    global CSV_ATENDIDO_POR
-    global CSV_SEVERIDADE
-    global CSV_PRIORIDADE
-    global CSV_CUSTOMER
-    global CSV_DATA_CRIACAO
-    global CSV_DATA_FECHAMENTO
-    global CSV_TIPO_CHAMADO
-
-    global FIELD_MAP
-    global DATE_FORMAT
-
-    # Carrega o arquivo json
-    with open(MAPPING_FILE) as f:
-        CONFIG_OBJECT = json.load(f)
-
-    # Mapeamento de campos
-    FIELD_MAP       = CONFIG_OBJECT["KeyMapping"]["FieldMapping"]
-    ID              = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["id"]
-    STATUS          = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["status"]
-    CRIADO_POR      = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["criado_por"]
-    ATRIBUIDO_A     = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["atribuido_a"]
-    ATENDIDO_POR    = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["atendido_por"]
-    SEVERIDADE      = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["severidade"]
-    PRIORIDADE      = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["prioridade"]
-    CLIENTE         = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["cliente"]
-    DATA_CRIACAO    = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["data_criacao"]
-    DATA_FECHAMENTO = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["data_fechamento"]
-    TIPO_CHAMADO    = CONFIG_OBJECT["KeyMapping"]["VarMapping"]["tipo_chamado"]
-    DATE_FORMAT     = CONFIG_OBJECT["DateFormat"]
-
-    key_list = list(FIELD_MAP.keys())
-    val_list = list(FIELD_MAP.values())
-
-    CSV_ID              = key_list[val_list.index(ID)]
-    CSV_STATUS          = key_list[val_list.index(STATUS)]
-    CSV_CRIADO_POR      = key_list[val_list.index(CRIADO_POR)]
-    CSV_ATRIBUIDO_A     = key_list[val_list.index(ATRIBUIDO_A)]
-    CSV_ATENDIDO_POR    = key_list[val_list.index(ATENDIDO_POR)]
-    CSV_SEVERIDADE      = key_list[val_list.index(SEVERIDADE)]
-    CSV_PRIORIDADE      = key_list[val_list.index(PRIORIDADE)]
-    CSV_CLIENTE         = key_list[val_list.index(CLIENTE)]
-    CSV_DATA_CRIACAO    = key_list[val_list.index(DATA_CRIACAO)]
-    CSV_DATA_FECHAMENTO = key_list[val_list.index(DATA_FECHAMENTO)]
-    CSV_TIPO_CHAMADO    = key_list[val_list.index(TIPO_CHAMADO)]
-
-microdados_date_cols = [
-    'DataNotificacao',
-    'DataCadastro',
-    'DataDiagnostico',
-    'DataColeta_RT_PCR',
-    'DataColetaTesteRapido',
-    'DataColetaSorologia',
-    'DataColetaSorologiaIGG',
-    'DataEncerramento',
-    'DataObito']    
-microdados_cat_cols = []
-microdados_num_cols = []
+    return(DTYPE_DICT)
 
 # ############################################################################
 # REGISTROS DAS FONTES DE DADOS UTILIZADAS
@@ -158,13 +58,10 @@ MICRODADOS = {
     "URLS": {
         "MICRODADOS": "https://bi.s3.es.gov.br/covid19/MICRODADOS.csv",
     },
-    "FORMAT": "csv",
+    "FORMAT": ".csv",
     "DELIMITER": ";",
     "ENCODING": "ISO-8859-1",
-    "MAP": { # data.load_json("MICRODADOS")
-        'date_cols': microdados_date_cols,
-        'cat_cols': microdados_cat_cols,
-    } 
+    "MAP": read_dtype_map("DTYPEMAP_MICRODADOS.json")
 }
 
 MICRODADOS_BAIRROS = {
@@ -174,13 +71,10 @@ MICRODADOS_BAIRROS = {
     "URLS": {
         "MICRODADOS_BAIRROS": "https://bi.s3.es.gov.br/covid19/MICRODADOS_BAIRROS.csv",
     },
-    "FORMAT": "csv",
+    "FORMAT": ".csv",
     "DELIMITER": ",",
     "ENCODING": "ISO-8859-1",
-    "MAP": { # data.load_json("MICRODADOS_BAIRROS")
-        'date_cols': [],
-        'cat_cols': [],
-    } 
+    "MAP": read_dtype_map("DTYPEMAP_MICRODADOS_BAIRROS.json")
 }
 
 TIPO_ARRECADACAO = {
@@ -190,13 +84,10 @@ TIPO_ARRECADACAO = {
     "URLS": {
         "TipoArrecadacao": "https://drive.economia.gov.br/owncloud/index.php/s/OEehiiL427jBp7C/download",
     },
-    "FORMAT": "csv",
+    "FORMAT": ".csv",
     "DELIMITER": ",",
     "ENCODING": "UTF-8",
-    "MAP": {
-        'date_cols': [],
-        'cat_cols': [],
-    }
+    "MAP": read_dtype_map("DTYPEMAP_TIPO_ARRECADACAO.json")
 }
 
 ARRECADACAO = {
@@ -210,13 +101,10 @@ ARRECADACAO = {
         "Arrecadacao_01-01-2010_a_31-12-2013": "https://drive.economia.gov.br/owncloud/index.php/s/BaUyR54HEzTHajy/download",
         "Arrecadacao_01-01-2014_a_31-12-2017": "https://drive.economia.gov.br/owncloud/index.php/s/FNJFQQtoRpPZJd1/download",
     },
-    "FORMAT": "csv",
+    "FORMAT": ".csv",
     "DELIMITER": ",",
     "ENCODING": "UTF-8",
-    "MAP": {
-        'date_cols': [],
-        'cat_cols': [],
-    }
+    "MAP": read_dtype_map("DTYPEMAP_ARRECADACAO.json")
 }
 
 TRANSFERENCIAS = {
@@ -237,13 +125,10 @@ TRANSFERENCIAS = {
         "TRANSFESTADOMUNICIPIOS-2020": "https://dados.es.gov.br/dataset/d85497f8-3dc4-4104-8e18-f242ae82b6ee/resource/511c02b1-93b7-421c-8727-d786f80fd8e5/download/transfestadomunicipios-2020.csv",
         "TRANSFESTADOMUNICIPIOS-2021": "https://dados.es.gov.br/dataset/d85497f8-3dc4-4104-8e18-f242ae82b6ee/resource/672f4fcc-c1aa-4c3d-b0a0-e4cd4e2695da/download/transfestadomunicipios-2021.csv",
     },
-    "FORMAT": "csv",
+    "FORMAT": ".csv",
     "DELIMITER": ";",
     "ENCODING": "UTF-8",
-    "MAP": {
-        'date_cols': [],
-        'cat_cols': [],
-    }
+    "MAP": read_dtype_map("DTYPEMAP_TRANSFERENCIAS.json")
 }
 
 POPULACAO = {
@@ -256,11 +141,8 @@ POPULACAO = {
         "POPULACAO_2020": os.path.join(DATADIR, "populacao_2020.csv"),
         "POPULACAO_2021": os.path.join(DATADIR, "populacao_2021.csv"),
     },
-    "FORMAT": "csv",
+    "FORMAT": ".csv",
     "DELIMITER": ",",
     "ENCODING": "UTF-8",
-    "MAP": {
-        'date_cols': [],
-        'cat_cols': [],
-    }
+    "MAP": read_dtype_map("DTYPEMAP_POPULACAO.json")
 }
