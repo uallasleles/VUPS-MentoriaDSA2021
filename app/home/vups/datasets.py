4d661903be75b62bedf4e140848ae9b9f8d68170
Uallas Leles <uallasleles@hotmail.com>
2021-10-19 11:43:17 -0300

    Recurso para criar DTYPE_MAP_{...}.json caso não exista, adicionado progress bar em terminial para download de csv e para importação de arquivo via método progress_read_csv().

--- app/home/vups/datasets.py
+++ app/home/vups/datasets.py
@@ -1,13 +1,23 @@
 # -*- encoding: utf-8 -*-
 """
-Programa de Mentoria DSA 2021
+******************************************************************************
+* MÓDULO: datasets
+******************************************************************************
 """
 
 # IMPORTAÇÕES ****************************************************************
-from app.home.vups import const
-import os, sys, validators, requests, time, pandas as pd
-# ############################################################################
+from typing import Iterable
+from tqdm.gui import tqdm_gui
+from app.home.vups import const, data, utils
+import os, sys, validators, requests, time, pandas as pd, inspect
+from clint.textui import progress
+from pathlib import Path
+import pandas as pd
+import tqdm
+import typer
+# ****************************************************************************
 
+# @utils.print_caller_name(4)
 def get_data(
     filepath_or_buffer=None,
     usecols=None,
@@ -19,139 +29,192 @@ def get_data(
     dtype=None,
     mapa=None,
     memory_map=True,
-    name=None
+    name=None,
+    format=None
 ):
-    """
-        FUNÇÃO CENTRAL PARA OBETENÇÃO DE DADOS
-        ----------------------------------------------------------------
-        BUSCA O DATASET POR ORDEM DE PRECEDÊNCIA DO TIPO DE ARMAZENAMENTO.
-            PRECEDÊNCIA: PARQUET < CSV < URL
-        - TENTA IMPORTAR O DATASET SOLICITADO DE ARQUIVO PARQUET PARA DATAFRAME DO PANDAS, CASO O PARQUET EXISTA
-        - SENÃO, TENTA IMPORTAR DE CSV PARA DATAFRAME DO PANDAS, TRATA OS DADOS E FAZ O CAST PARA PARQUET, UTILIZANDO POR FIM O PARQUET
-        - CASO NÃO ENCONTRE O DATASET EM CSV, RECORRE AS URLS NO REGISTRO DO DATASET
-        AS URLS (CSV) SÃO IMPORTADAS PARA DATAFRAME DO PANDAS, OS DADOS SÃO TRATADOS 
-        ENTÃO, É FEITO UM CAST PARA PARQUET, O ARQUIVO É GRAVADO EM DISCO, POR FIM O PARQUET É UTILIZADO
+    """FUNÇÃO CENTRAL PARA OBTENÇÃO DE DADOS
+    ----------------------------------------------------------------------
+    - BUSCA O DATASET POR ORDEM DE PRECEDÊNCIA DO TIPO DE ARMAZENAMENTO.
+        PRECEDÊNCIA: PARQUET < CSV < URL
+    - TENTA IMPORTAR O DATASET SOLICITADO DE PARQUET FILE PARA DATAFRAME DO PANDAS, CASO O PARQUET EXISTA.
+    - SENÃO, TENTA IMPORTAR DE CSV PARA DATAFRAME DO PANDAS, TRATA OS DADOS E FAZ O CAST PARA PARQUET, UTILIZANDO POR FIM O PARQUET
+    - CASO NÃO ENCONTRE O DATASET EM CSV, RECORRE AS URLS NO REGISTRO DO DATASET
+    AS URLS (CSV) SÃO IMPORTADAS PARA DATAFRAME DO PANDAS, OS DADOS SÃO TRATADOS, 
+    CONVERTIDO PARA PARQUET, O ARQUIVO É GRAVADO EM DISCO, POR FIM O PARQUET É UTILIZADO.
     """
     
     filename, file_extension = os.path.splitext(os.path.basename(filepath_or_buffer[0][1]))
-    print("Importando o dataset {}...".format(filename))
+    chunk_size = 1024
 
     if validators.url(filepath_or_buffer[0][1]):
-        print("Buscando dataset de URL...")
-        file_extension = '.csv'
+        print("Baixando {} de URL ...".format(filename))
+
         filepath = []
         for url in filepath_or_buffer:
             filename = url[0]
             if validators.url(url[1]):
                 r = requests.get(url[1], stream=True)
 
+                # CRIA UM PATH PARA GRAVAR O ARQUIVO BAIXADO E, ADICIONA O PATH A UMA LISTA
                 DATENAME = time.strftime("%Y%m%d-%H%M%S")
-                filepath.append(os.path.join(const.DATADIR, "{}-{}{}".format(DATENAME, filename.upper(), file_extension)))
-                print("Gravando os dados em disco...")
-                with open(filepath[-1], 'wb') as fd:
-                    for chunk in r.iter_content(chunk_size=128):
-                        fd.write(chunk)
+                filepath.append(
+                    os.path.join(
+                        const.DATATMP, 
+                        "{}-{}{}".format(DATENAME, filename.upper(), format)
+                        )
+                    )
 
+                # GRAVA CADA DATASET BAIXADO, PARA DEPOIS ... 
+                # ... CONCATENÁ-LOS, TRANSFORMAR OS TIPOS DE DADOS E CONVERTER O DATASET UNIFICADO EM PARQUET
+               
+                with open(filepath[-1], 'wb') as fd:
+                    print("Obtendo o comprimento dos dados ...")
+                    total_length = int(r.headers.get('content-length'))
+
+                    print("Gravando os dados em disco ...")
+                    for chunk in progress.bar(r.iter_content(chunk_size=chunk_size), expected_size=(total_length/chunk_size) + 1):
+                        if chunk:
+                            fd.write(chunk)
+                            fd.flush()
+
+        # OS DATASETS BAIXADOS SÃO IMPORTADOS NOVAMENTE DOS ARQUIVOS EM DISCO
         ds_lst = []
         for f in filepath:
             _PARAMS = {
                 "filepath_or_buffer": f,
                 "iterator": True,
-                "chunksize": 128,
+                "chunksize": chunk_size,
                 "usecols": usecols,
                 "sep": sep,
                 "nrows": nrows,
                 "encoding": encoding,
                 "warn_bad_lines": warn_bad_lines,
                 "error_bad_lines": error_bad_lines,
-                "parse_dates": True,
-                "dtype": dtype,
+                # "parse_dates": True,
+                # "dtype": dtype,
                 "memory_map": memory_map
             }
-
             iter = pd.read_csv(**_PARAMS)
             subset = pd.concat(iter, ignore_index=True)
+            # (CHAMO CADA CONJUNTO QUE COMPÕE O MESMO DATASET DE "SUBSET")
+
+            # OS "SUBSETS" SÃO ADICIONADOS EM UMA LISTA PARA SEREM CONCATENADOS
             ds_lst.append(subset)
 
         print("Concatenando arquivos, caso sejam múltiplos datasets...")
         dataset = pd.concat(ds_lst, ignore_index=True)
 
-        print(mapa)
-        if mapa != "{}":
+        # AGORA É A HORA DO TRATAMENTO E TRANSFORMAÇÃO DOS DADOS, PARA NO FIM GRAVAR UM PARQUET TIPADO
+        # --------------------------------------------------------------------------------------------
+
+        # USO O DTYPE MAP QUE FOI DECLARADO NO REGISTRO DO DATASET
+        if mapa is not None:
             print("Realizando transformações de dados no dataset consolidado...")
             dataset = data.dtype_transform(dataset, mapa)
         else:
+            # CASO NÃO EXISTA O DTYPE MAP, UM INICIAL É CRIADO PARA FUTURAMENTE SER CONFIGURADO E O TRATAMENTO REPROCESSADO
             print("Inicializando mapeamento de tipos de dados...")
-            data.field_map(dataset, name)
+            data.create_dtype_map(dataset, name)
         
+        # APÓS O TRATAMENTO E TRANSFORMAÇÃO DOS DADOS, UM ARQUIVO PARQUET É CRIADO, PASSANDO A SER O DATASET OFICIAL
         print("Convertendo para PARQUET o dataset com os tipos de dados já tratados...")
-        file_extension = data.convert_to_parquet([dataset], name)
+        filepath_or_buffer = data.convert_to_parquet([dataset], name)
+        file_extension = ".parquet"
 
-        filepath_or_buffer = {"filepath": {"ext": os.path.join(const.DATADIR, "{}{}".format(name.upper(), file_extension))}}
-        filepath_or_buffer = list(filepath_or_buffer.get("filepath").items())
+        # LIMPA O DIRETÓRIO DE DADOS TEMPORÁRIOS    # ALTERNATIVA
+        # for f in os.listdir(const.DATATMP):         # for f in filepath:
+        #     file = os.path.join(const.DATATMP, f)   #     REMOVE APENAS OS ARQUIVOS USADOS NESTA IMPORTAÇÃO
+        #     os.remove(file)                         #     os.remove(f)
 
     if file_extension == ".csv":
+        # print("Importando {} de CSV...".format(filename))
 
         _PARAMS = {
             "filepath_or_buffer": filepath_or_buffer[0][1],
             "iterator": True,
-            "chunksize": 128,
+            "chunksize": chunk_size,
             "usecols": usecols,
             "sep": sep,
             "nrows": nrows,
             "encoding": encoding,
             "warn_bad_lines": warn_bad_lines,
             "error_bad_lines": error_bad_lines,
-            "parse_dates": True,
-            "dtype": dtype,
-            "memory_map": memory_map
+            # "parse_dates": True,
+            # "dtype": dtype,
+            "memory_map": memory_map,
+            "low_memory": False
         }
 
-        iter = pd.read_csv(**_PARAMS)
-        dataset = pd.concat(iter, ignore_index=True)
+        dataset = progress_read_csv(**_PARAMS)
+        # iter = progress_read_csv(**_PARAMS)
+        # dataset = pd.concat(iter, ignore_index=True)
         
-        print(mapa)
-        if mapa != "{}":
-            print("Realizando transformações de dados no dataset consolidado...")
+        if mapa is not None:
+            # print("Realizando transformações de dados no dataset CSV unificado...")
+            # print("MAPA:", mapa)
             dataset = data.dtype_transform(dataset, mapa)
         else:
-            print("Inicializando mapeamento de tipos de dados...")
-            data.dump.field_map(dataset, name)
+            # print("Inicializando as variáveis do mapeamento de tipos de dados...")
+            # print("MAPA:", mapa)
+            data.create_dtype_map(dataset, name)
 
-        dataset = data.dtype_transform(dataset, mapa)
-        file_extension = data.convert_to_parquet([dataset], name)
-        filepath_or_buffer = data.which_file_exists(name)
-        
+        filepath_or_buffer = data.convert_to_parquet([dataset], name)
+        file_extension = ".parquet"
+
     if file_extension == ".parquet":
-        print("Realizando importação de arquivo PARQUET.")
+        # print("Importando {} de PARQUET...".format(filename))
+
         _PARAMS = {
             "path": filepath_or_buffer[0][1],
-            "columns": usecols
+            "columns": usecols,
         }
         dataset = pd.read_parquet(**_PARAMS)
-            
+
+        # PATH = const.METADIR
+        # FILENAME = "DTYPEMAP_{}.json".format(name)
+        # FILEPATH = os.path.join(PATH, FILENAME)
+        
+        # if not os.path.exists(FILEPATH):
+        #     print("NÃO EXISTE MAPA!")
+        #     data.create_dtype_map(dataset, name)
+        # elif mapa != const.read_dtype_map(dataset):
+        #     print("MAPAS DIFERENTES! REPROCESSANDO O TRATAMENTO DE DADOS ...")
+        #     dataset = data.dtype_transform(dataset, mapa)
+        #     print("ATUALIZANDO O ARQUIVO PARQUET ...")
+        #     data.convert_to_parquet([dataset], name)
+
+    # if filename.strip() != "":
+    #     curframe = inspect.currentframe()
+    #     calframe = inspect.getouterframes(curframe, 2)
+    #     print("===================================================================")
+    #     print("FILENAME:", filename)
+    #     print("# FILE: {}".format(__file__))
+    #     print("# NAME: {}".format(__name__))
+    #     print("# OBJECT: {}".format(sys._getframe(  ).f_code.co_name))
+    #     print("# CALLER:", calframe[1][3])
+    #     print("-------------------------------------------------------------------")
+    #     print(dataset.info())
+    #     print("===================================================================")
+
     return dataset
 
-class datasets:
-    """
-    CLASSE QUE INTERMEDIA A PASSAGEM DE PARÂMETROS DOS DATASETS REGISTRADOS
+
+class Datasets():
+    """CLASSE QUE INTERMEDIA A PASSAGEM DE PARÂMETROS DOS DATASETS REGISTRADOS
     OS METADADOS E CONFIGURAÇÕES DE TODOS OS DATASETS SÃO ARMAZENADOS EM ARQUIVOS JSON
     OS ARQUIVOS JSON SÃO LIDOS PELO MÓDULO [vups.const]
     OS MÉTODOS DESTA CLASSE BUSCAM NO MÓDULO [vups.const] OS PARÂMETROS DO DATASET A SER IMPORTADO
     ENTÃO, REPASSA ESSES PARÂMETROS PARA O MÉTODO get_data()
     """
-    
+
     def microdados(columns=None, nrows=None, dtype=None):
-        """
-        MÉTODO QUE BUSCA OS PARÂMETROS DO DATASET [microdados]
-        """
+        """MÉTODO QUE BUSCA OS PARÂMETROS DO DATASET [microdados]"""
 
         # SHOW DE BOLA ESSA PASSAGEM DINÂMICA DE NAMESPACES! ^^
         name = sys._getframe(  ).f_code.co_name.upper()
         filepath_or_buffer = data.which_file_exists(name)
-
         mapa = getattr(const, name).get("MAP")
-        print(mapa)
+        format = getattr(const, name).get("FORMAT")
 
         return get_data(
             filepath_or_buffer=filepath_or_buffer,
@@ -163,13 +226,15 @@ class datasets:
             error_bad_lines=False,
             dtype=dtype,
             mapa=mapa,
-            name=name
+            name=name,
+            format=format
         )
 
     def microdados_bairros(columns=None, nrows=None, dtype=None):
         name = sys._getframe(  ).f_code.co_name.upper()
         filepath_or_buffer = data.which_file_exists(name)
         mapa = getattr(const, name).get("MAP")
+        format = getattr(const, name).get("FORMAT")
 
         return get_data(
             filepath_or_buffer=filepath_or_buffer,
@@ -181,13 +246,15 @@ class datasets:
             error_bad_lines=False,
             dtype=dtype,
             mapa=mapa,
-            name=name
+            name=name,
+            format=format
         )
 
     def arrecadacao(columns=None, nrows=None, dtype=None):
         name = sys._getframe(  ).f_code.co_name.upper()
         filepath_or_buffer = data.which_file_exists(name)
         mapa = getattr(const, name).get("MAP")
+        format = getattr(const, name).get("FORMAT")
 
         return get_data(
             filepath_or_buffer=filepath_or_buffer,
@@ -199,13 +266,15 @@ class datasets:
             error_bad_lines=False,
             dtype=dtype,
             mapa=mapa,
-            name=name
+            name=name,
+            format=format
         )
 
     def tipo_arrecadacao(columns=None, nrows=None, dtype=None):
         name = sys._getframe(  ).f_code.co_name.upper()
         filepath_or_buffer = data.which_file_exists(name)
         mapa = getattr(const, name).get("MAP")
+        format = getattr(const, name).get("FORMAT")
 
         return get_data(
             filepath_or_buffer=filepath_or_buffer,
@@ -217,13 +286,15 @@ class datasets:
             error_bad_lines=False,
             dtype=dtype,
             mapa=mapa,
-            name=name
+            name=name,
+            format=format
         )
 
     def transferencias(columns=None, nrows=None, dtype=None):
         name = sys._getframe(  ).f_code.co_name.upper()
         filepath_or_buffer = data.which_file_exists(name)
         mapa = getattr(const, name).get("MAP")
+        format = getattr(const, name).get("FORMAT")
 
         return get_data(
             filepath_or_buffer=filepath_or_buffer,
@@ -235,13 +306,15 @@ class datasets:
             error_bad_lines=False,
             dtype=dtype,
             mapa=mapa,
-            name=name
+            name=name,
+            format=format
         )
 
     def populacao(columns=None, nrows=None, dtype=None):
         name = sys._getframe(  ).f_code.co_name.upper()
         filepath_or_buffer = data.which_file_exists(name)
         mapa = getattr(const, name).get("MAP")
+        format = getattr(const, name).get("FORMAT")
 
         return get_data(
             filepath_or_buffer=filepath_or_buffer,
@@ -253,5 +326,43 @@ class datasets:
             error_bad_lines=False,
             dtype=dtype,
             mapa=mapa,
-            name=name
+            name=name,
+            format=format
         )
+
+def progress_read_csv(**_PARAMS):
+
+    # read number of rows quickly
+    filename = _PARAMS.get('filepath_or_buffer')
+    length = sum1forline(filename)
+    chunksize = _PARAMS.get('chunksize')
+
+    # initiate a blank dataframe
+    df = pd.DataFrame()
+
+    # fancy logging with typer
+    typer.secho(f"LENDO ARQUIVO: {filename}", fg="red", bold=True)
+    typer.secho(f"TOTAL DE LINHAS: {length}", fg="green", bold=True)
+
+    # tqdm context
+    with tqdm.tqdm(total=length, desc="REGISTROS LIDOS") as bar:
+        lst = []
+        # enumerate chunks read without low_memory (it is massive for pandas to precisely assign dtypes)
+        for i, chunk in enumerate(pd.read_csv(**_PARAMS)):
+            
+            # append it to df
+            # df = df.append(chunk)
+            lst.append(chunk)
+            # update tqdm progress bar
+            bar.update(chunksize)
+    df = pd.concat(lst, ignore_index=True)
+    print(df.info())
+
+    # finally inform with a friendly message
+    typer.secho("FIM DA LEITURA DOS REGISTROS...", fg=typer.colors.BRIGHT_RED)
+    typer.secho(f"COMPRIMENTO:{len(df)}", fg="green", bold=True)
+    return df
+
+def sum1forline(filename):
+    with open(filename) as f:
+        return sum(1 for line in f)
