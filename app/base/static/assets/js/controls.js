fc32259c52fad5175f73d6aa63b49602f6852544
Uallas Leles <uallasleles@hotmail.com>
2021-11-06 13:45:13 -0300

    Página de preview para datasets

---
+++ app/base/static/assets/js/controls.js
@@ -0,0 +1,15 @@
+// cria um objeto que possui uma seleção de elementos com essa classe dropdown-item
+var oDropdownItem = document.getElementsByClassName("dropdown-item");
+// percorre os índices das opções
+for(i in oDropdownItem){
+    // trata o texto de cada opção, remove quebras de linha e espaços
+    let item = oDropdownItem[i].text.replace(/^\s+|\s+$/gm,'').trim();
+    // adiciona uma espera de evento onclick para cada opção do dropdown
+    oDropdownItem[i].addEventListener("click", function(){ // função de callback
+        
+        // processamento quando o click ocorrer
+        tgt = "/tbl_"+item+".html"; // monta uma url
+        window.location.href = tgt; // faz o redirecionamento
+    
+    });
+};
