493518b66696b335420998bec71003f6c0ffe60d
Uallas Leles <uallasleles@hotmail.com>
2021-11-18 02:56:30 -0300

    node+npm+gulp+sass

--- app/base/static/assets/gulpfile.js
+++ app/base/static/assets/gulpfile.js
@@ -1,59 +1,80 @@
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
-var cleanCss = require('gulp-clean-css');
-var del = require('del');
-const htmlmin = require('gulp-htmlmin');
-const cssbeautify = require('gulp-cssbeautify');
-var gulp = require('gulp');
-const npmDist = require('gulp-npm-dist');
-var sass = require('gulp-sass')(require('node-sass'));
-var wait = require('gulp-wait');
-var sourcemaps = require('gulp-sourcemaps');
-var fileinclude = require('gulp-file-include');
-var rename = require("gulp-rename");
+var gulp            = require('gulp');
+var browserSync     = require('browser-sync').create();     // adiciona prefixos de fornecedor ao CSS como  -webkit- , -ms- , etc.
+// var autoprefixer = require('gulp-autoprefixer');
+var del             = require('del');
+var sass            = require('gulp-sass')(require('node-sass'));
+var cleanCss        = require('gulp-clean-css');
+const cssbeautify   = require('gulp-cssbeautify');
+var concat          = require('gulp-concat');
+var terser          = require('gulp-terser');
+const htmlmin       = require('gulp-htmlmin');
+const npmDist       = require('gulp-npm-dist');     // listar as dependências do package.json e copiar os arquivos dist deles para uma pasta específica
+var wait            = require('gulp-wait');                        
+var sourcemaps      = require('gulp-sourcemaps');   // indica se um mapa de origem para o arquivo empacotado deve ser gerado ou não. O valor padrão é falso.
+                                                    // mapeia os estilos CSS de volta ao arquivo SCSS original nas ferramentas de desenvolvimento do seu navegador
+var fileinclude     = require('gulp-file-include');
+var rename          = require("gulp-rename");
 
-// Define paths
+// Definir caminhos
 const paths = {
-    dist: {
-        base: './dist/',
-        css: './dist/css',
-        html: './dist/pages',
+    dist: { // distribution
+        base:   './dist/',
+        css:    './dist/css',
+        js:     './dist/js',
+        html:   './dist/pages',
         assets: './dist/assets',
-        img: './dist/assets/img',
+        img:    './dist/assets/img',
         vendor: './dist/vendor'
     },
-    dev: {
-        base: './html&css/',
-        css: './html&css/css',
-        html: './html&css/pages',
+    dev: { // developement
+        base:   './html&css/',
+        css:    './html&css/css',
+        html:   './html&css/pages',
         assets: './html&css/assets',
-        img: './html&css/assets/img',
+        img:    './html&css/assets/img',
         vendor: './html&css/vendor'
     },
     base: {
         base: './',
         node: './node_modules'
     },
-    src: {
-        base: './',
-        css: './css',
-        html: './src/pages/**/*.html',
-        assets: './src/assets/**/*.*',
-        partials: './src/partials/**/*.html',
-        scss: './scss',
-        node_modules: './node_modules/',
-        vendor: './vendor'
+    src: { // source
+        base:           './',
+        css:            './css',
+        js:             './js',
+        html:           './src/pages/**/*.html',
+        assets:         './src/assets/**/*.*',
+        partials:       './src/partials/**/*.html',
+        scss:           './scss',
+        node_modules:   './node_modules/',
+        vendor:         './vendor'
     },
     temp: {
-        base: './.temp/',
-        css: './.temp/css',
-        html: './.temp/pages',
+        base:   './.temp/',
+        css:    './.temp/css',
+        js:    './.temp/js',
+        html:   './.temp/pages',
         assets: './.temp/assets',
         vendor: './.temp/vendor'
     }
 };
 
-// Compile SCSS
+// Tarefa padrão
+function defaultTask(cb) {
+    // coloque o código para sua tarefa padrão aqui
+    cb();
+};
+exports.defaultTask = defaultTask;
+
+// Copiar arquivos
+function copy(cp){
+    gulp.src(paths.src.js + '/*.js')
+    .pipe(gulp.dest(paths.dist.js));
+    cp();
+};
+exports.copy = copy;
+
+// Compilar SASS
 gulp.task('scss', function() {
     return gulp.src([
         paths.src.scss + '/custom/**/*.scss', 
@@ -62,15 +83,15 @@ gulp.task('scss', function() {
         .pipe(wait(500))
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['> 1%']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.dist.css))
-        .pipe(browserSync.stream());
+        .pipe(sourcemaps.write('../css'))
+        .pipe(gulp.dest(paths.src.css))
+        .pipe(browserSync.stream()); // injetar alterações sem atualizar a página
+        // isso é usado em CSS e outros formatos de folha de estilo
+        // é útil porque mantém a posição de rolagem intacta 
+        // e não leva você para o topo da página como uma atualização de página normalmente faria
 });
 
-// Minify CSS
+// Minificar CSS
 gulp.task('minify:css', function() {
     return gulp.src([
             paths.src.css + '/volt.css'
@@ -78,10 +99,45 @@ gulp.task('minify:css', function() {
         .pipe(cleanCss())
         .pipe(rename(function(path) {
             // Updates the object in-place
+            path.basename = "style";
             path.extname = ".min.css";
         }))
         .pipe(gulp.dest(paths.dist.css))
 });
 
-// Default
-gulp.task('default', gulp.series('scss', 'minify:css'));
+// Minificar JS
+gulp.task('minify:js', function() {
+    return gulp.src([
+            paths.src.js + '/*.js'
+        ])
+        .pipe(concat('all.js'))
+        .pipe(terser())
+        .pipe(rename(function(path) {
+            // Updates the object in-place
+            path.extname = ".min.js";
+        }))
+        .pipe(gulp.dest(paths.dist.js))
+});
+
+// Minificar HTML
+gulp.task('minify:html', function() {
+    return gulp.src([
+        paths.src.html + "/*.html"])
+        .pipe(wait(500))
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest(paths.dist.html))
+})
+
+gulp.task('copy:libs', function() {
+    return gulp.src(
+        npmDist(), {base:'./node_modules/'})
+        .pipe(sourcemaps.init())
+        .pipe(rename(function(path) {
+            path.dirname = path.dirname.replace(/\/dist/, '').replace(/\\dist/, '');
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.dist.vendor));
+});
+
+// Build
+gulp.task('build', gulp.series('scss', 'minify:css', 'minify:js', 'minify:html', 'copy:libs'));
